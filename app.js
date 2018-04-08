e35c0029dfca5c27653eaf7eb73d44bfa2c50a83
Suraj Gusain <suraj.gusain15@gmail.com>
2018-04-08 22:00:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var appRouter = require('./routes/app');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', appRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.render('index');
+});
+
+module.exports = app;
